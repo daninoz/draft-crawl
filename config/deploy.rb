fb6952604aeb2e9a747390021e0943355b7d32ff
Daniel Albornoz <dalbornoz89@gmail.com>
2014-01-31 19:48:08 -0300

    fixes

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,8 @@ set :repo_url, 'git@github.com:daninoz/draft-crawl.git'
 
 set :branch, 'master'
 
+set :linked_dirs, %w{app/config/production}
+
 set :deploy_to, '/var/www/vhosts/draft.daninoz.com'
 set :deploy_via, :remote_cache
 set :scm, :git
